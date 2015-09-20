79642c1a437172f680d7c7a60ca431d93ab6af94
Dennis Ruhe <dennis@amazingsystems.nl>
2015-09-20 17:09:05 +0200

    Electrum server works

---
+++ bin/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd "$(dirname $0)/.."
+
+docker build --rm -t eguldenfoundation/egulden-electrum .
---
+++ bin/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+cd "$(dirname $0)/.."
+
+source .env
+
+IMAGE_NAME="eguldenfoundation/egulden-electrum"
+CONTAINER_NAME="egulden-electrum"
+EGULDEND_NODE_CONTAINER_NAME="eguldend"
+
+START="\
+	docker run -it
+		--link $EGULDEND_NODE_CONTAINER_NAME \
+		-e EGULDEND_HOST_IP=$EGULDEND_HOST_IP \
+		-e EGULDEND_HOST_RPC_PORT=$EGULDEND_HOST_RPC_PORT \
+		-e EGULDEND_RPC_USER=$EGULDEND_RPC_USER \
+		-e EGULDEND_RPC_PASSWORD=$EGULDEND_RPC_PASSWORD \
+		-p 50001:50001
+		-d --name=$CONTAINER_NAME \
+		"
+
+set -x
+exec $START $IMAGE_NAME $@
